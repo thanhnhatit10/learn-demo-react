7d9e87cc03f1eb24bfdfa79b066b162d8655c852
Nhat Nguyen <thanhnhatphanthuys@gmail.com>
2021-05-18 03:38:21 +0700

    Commit new

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Route, Switch, useRouteMatch} from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import ListPage from './pages/ListPage';
 import { Box } from '@material-ui/core';
 
 ProductsFeature.propTypes = {
-    
+
 };
 
 function ProductsFeature(props) {
     const match = useRouteMatch();
     return (
-        <Box pt={4}>
+        <Box pt={5}>
             <Switch>
-                <Route path={match.url} component={ListPage}/>
+                <Route path={match.url} component={ListPage} />
                 {/* <Route path={`${match.path}/:productId`} component={}/> */}
             </Switch>
         </Box>
